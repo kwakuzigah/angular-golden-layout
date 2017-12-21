0ac90da267a51cfff46645f2dbed2e8d88f6bed4
Kwaku Zigah <kwaku.zigah@nowinstore.com>
2017-12-21 18:13:34 -0500

    added demo and components

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export { GoldenLayoutComponent } from './lib/golden-layout.component';
+export { GoldenLayoutDirective } from './lib/golden-layout.directive';
+
+export {
+  GOLDEN_LAYOUT_CONFIG,
+
+  GoldenLayoutConfig
+
+//   DropzoneUrlFunction,
+//   DropzoneMethodFunction,
+//   DropzoneParamsFunction,
+//   DropzoneHeadersFunction,
+
+//   DropzoneInitFunction,
+//   DropzoneFallbackFunction,
+//   DropzoneAcceptFunction,
+//   DropzoneResizeFunction,
+//   DropzoneRenameFileFunction,
+//   DropzoneTransformFileFunction,
+//   DropzoneChunksUploadedFunction
+} from './lib/golden-layout.interfaces';
+
+export { GoldenLayoutModule } from './lib/golden-layout.module';
